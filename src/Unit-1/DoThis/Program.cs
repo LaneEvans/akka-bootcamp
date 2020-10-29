83f9d77f4baff8f1d39d397cefcc59561085e11a
laneevans <laneevans@outlook.com>
2020-10-28 19:52:24 -0500

    Completed Unit 1 Lesson 3

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -11,10 +11,16 @@ namespace WinTail
         {
             // make an actor system 
             _myActorSystem = ActorSystem.Create("MyActorSystem");
-            
+
             // make our first actors!
-            var consoleWriterActor = _myActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
-            var consoleReaderActor = _myActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
+            var consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            var consoleWriterActor = _myActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
+
+            var validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            var validationActor = _myActorSystem.ActorOf(validationActorProps, "validationActor");
+
+            var consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+            var consoleReaderActor = _myActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
 
             // tell console reader to begin
             consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
